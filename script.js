ad3f2901a2e22cb1b4433531e4b90d0fe497d872
Sachin Alam <sachinalam4216@gmail.com>
2024-10-15 12:28:21 +0530

    final version

---
+++ script.js
@@ -0,0 +1,30 @@
+// DOM Selectors
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("add");
+const ul = document.getElementById("taskList");
+
+addBtn.addEventListener("click", addTask);
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+  console.log(taskInput.value);
+
+  if (taskText === "") {
+    alert("Enter task");
+    return;
+  }
+
+  let li = document.createElement("li");
+  li.textContent = taskText;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i> Delete';
+  deleteButton.className = "delete";
+  deleteButton.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(deleteButton);
+  ul.appendChild(li);
+  taskInput.value = "";
+}
